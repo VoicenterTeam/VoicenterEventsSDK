a38c2b1004016ce38860443856ee991c62fb26bf
cristij <joracristi@gmail.com>
2019-06-27 11:45:28 +0300

    Add config for webstorm alias imports, add sample widget component and grid for it

---
+++ src/components/WidgetCard.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="bg-white px-6 py-4 flex items-center rounded-lg shadow">
+        <slot name="icon">
+            <img class="w-16 mr-4" :src="icon" :alt="icon">
+        </slot>
+        <slot name="title">
+            <h5 class="text-3xl font-bold mr-4" v-if="title">
+                {{title}}
+            </h5>
+        </slot>
+        <slot name="description">
+            <p class="text-gray-500 text-lg" v-if="description">{{description}}</p>
+        </slot>
+    </div>
+</template>
+<script>
+  export default {
+    props: {
+      icon: String,
+      title: String,
+      description: String,
+    }
+  }
+</script>
+<style>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,35 @@
 <template>
-  <div>
-    Hi
-  </div>
+    <div class="mt-10">
+        <div class="flex -mx-2">
+            <div class="w-full lg:w-1/3 px-2">
+                <WidgetCard icon="/img/svg/phone.svg"
+                            title="16"
+                            description="Telephone Numbers">
+                </WidgetCard>
+            </div>
+            <div class="w-full lg:w-1/3 px-2">
+                <WidgetCard icon="/img/svg/phone.svg"
+                            title="25"
+                            description="Telephone Numbers">
+                </WidgetCard>
+            </div>
+            <div class="w-full lg:w-1/3 px-2">
+                <WidgetCard icon="/img/svg/phone.svg"
+                            title="115"
+                            description="Telephone Numbers">
+                </WidgetCard>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'home'
-}
+  import WidgetCard from '@/components/WidgetCard'
+
+  export default {
+    name: 'home',
+    components: {
+      WidgetCard
+    }
+  }
 </script>
