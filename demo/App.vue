0c811e471b7d1e68eab1b580dc7312e8aba333f9
cristij <joracristi@gmail.com>
2019-07-01 20:29:51 +0300

    Add ability to insert a monitor token

--- demo/App.vue
+++ demo/App.vue
@@ -1,8 +1,18 @@
 <template>
-  <div id="app" class="bg-gray-100 min-h-screen p-20">
+  <div id="app" class="bg-gray-100 min-h-screen px-20 py-8">
+    <div class="flex mb-3 w-full items-end">
+      <div class="flex-1">
+        <label>Monitor Code</label>
+        <input class="h-10 p-4 border rounded outline-none text-lg w-full"
+               type="text"
+               placeholder="Monitor code"
+               v-model="monitorCode">
+      </div>
+      <button class="px-3 p-2 bg-blue-400 text-white shadow rounded-r" @click="login">Login</button>
+    </div>
     <h1 class="text-4xl">Real Time Events</h1>
     <transition-group type="transition" name="list">
-      <div v-for="event in events" :key="event.timestamp"
+      <div v-for="event in events" :key="event.timestamp.getTime()"
            class="bg-white shadow-lg rounded-lg p-8 flex flex-col mb-5">
         <div class="flex w-full justify-between">
           <div class="text-xl text-green-500 mb-3">{{event.name}}</div>
@@ -12,8 +22,11 @@
         <tree-view :data="event.data" :options="{ maxDepth: 0, rootObjectKey: 'data'}"></tree-view>
       </div>
     </transition-group>
-    <div v-if="events.length === 0" class="text-red-500 text-2xl">
+    <div v-if="events.length === 0 && !error" class="text-blue-500 text-2xl">
       Waiting for events...
+    </div>
+    <div v-if="error" class="text-red-500 text-2xl">
+      {{error}}
     </div>
   </div>
 </template>
@@ -24,20 +37,33 @@ export default {
   name: 'app',
   data() {
     return {
-      events: []
+      events: [],
+      monitorCode: 'FWsqFkPDsJuuh54YU0VLCvTu5EIrplkrUFVQ7yMiXh7U52hfiNOYanNINtekWBb6X06C3LQR7jfgCCWGAbKKCdBJAeQFIWO3DCCl',
+      error: ''
+    }
+  },
+  methods: {
+    async login() {
+      this.events = []
+      this.error = ''
+      try {
+        let sdk = new EventsSdk({
+          token: this.monitorCode
+        })
+        await sdk.login()
+        sdk.on('*', data => {
+          this.events.unshift({
+            timestamp: new Date(),
+            ...data
+          })
+        })
+      } catch (e) {
+        this.error = e
+      }
     }
   },
   async mounted() {
-    let sdk = new EventsSdk({
-      token: "FWsqFkPDsJuuh54YU0VLCvTu5EIrplkrUFVQ7yMiXh7U52hfiNOYanNINtekWBb6X06C3LQR7jfgCCWGAbKKCdBJAeQFIWO3DCCl"
-    })
-    let res = await sdk.login()
-    sdk.on('*', data => {
-      this.events.unshift({
-        timestamp: new Date(),
-        ...data
-      })
-    })
+    this.login()
   }
 };
 </script>
