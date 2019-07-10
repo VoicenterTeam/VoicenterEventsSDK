17a8242c3ee45b91980ae623bb0631e7a866f867
ceciliaman <cecilia.man@binarcode.eu>
2019-07-10 12:44:45 +0300

    [RTD-9] Changes after pull request

--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -1,15 +1,16 @@
 <template>
-    <div class="bg-white p-10 rounded-lg shadow">
-        <div class="flex justify-end">
+    <div class="bg-white py-10 rounded-lg shadow">
+        <div class="flex justify-end mx-4">
             <div class="my-4">
                 <el-select
                         v-model="columnList"
                         value-key="prop"
                         multiple
                         filterable
+                        collapse-tags
                         default-first-option
                         size="large"
-                        placeholder="Choose tags for your article">
+                        :placeholder="$t('common.selectColumn')">
                     <el-option
                             v-for="item in optionColumns"
                             :key="item.value"
@@ -21,6 +22,8 @@
         </div>
         <el-table ref="table"
                   stripe
+                  row-key="id"
+                  :key="tableKey"
                   :data="tableData"
                   v-bind="$attrs"
                   v-on="listeners">
@@ -32,6 +35,16 @@
                         :min-width="column.minWidth || '150px'"
                         :fixed="column.fixed || false"
                         :type="column.type">
+                    <template slot="header">
+                        <span class="flex items-center" @mouseenter="hoverOverHeader(column)"
+                        @mouseleave="hoverOverHeader(column)">
+                            {{column.label}}
+                        </span>
+                        <span class="flex justify-end w-3/6" v-if="column.edit">
+                            <more-vertical-icon class="flex align-center w-4 h-4 -mx-1"></more-vertical-icon>
+                            <more-vertical-icon class="flex align-center w-4 h-4 -mx-2"></more-vertical-icon>
+                        </span>
+                    </template>
                     <template slot-scope="{row, $index}">
                         <slot :name="column.prop || column.type || column.label"
                               :row="row"
@@ -48,15 +61,20 @@
 <script>
     import {Table, TableColumn, Select, Option} from 'element-ui';
     import cloneDeep from 'lodash/cloneDeep'
+    import Sortable from 'sortablejs';
+    import {MoreVerticalIcon} from 'vue-feather-icons'
     export default {
         name: "data-table",
         data(){
             return{
                 columnList: cloneDeep(this.columns),
-                optionColumns: cloneDeep(this.columns)
+                optionColumns: cloneDeep(this.columns),
+                tableKey: 'table-key',
+                active: false
             }
         },
         components:{
+            MoreVerticalIcon,
             [Select.name]: Select,
             [Option.name]: Option,
             [Table.name]: Table,
@@ -90,10 +108,42 @@
             }
         },
         methods: {
+            hoverOverHeader(column){
+                this.$set(column, 'edit', !column.edit)
+            },
+            tryInitSortable () {
+                const table = this.$el.querySelector('.el-table__header-wrapper thead tr')
+                const self = this
+                Sortable.create(table, {
+                    onEnd ({ newIndex, oldIndex }) {
+                        const targetRow = self.columnList.splice(oldIndex, 1)[0]
+                        self.columnList.splice(newIndex, 0, targetRow)
+                        self.tableKey = self.columnList.map(c => c.prop).join('_')
+                        self.$nextTick(self.tryInitSortable)
+                    }
+                })
+            }
+        },
+        watch:{
+            columns(value){
+                this.columnList = cloneDeep(value);
+                this.optionColumns = cloneDeep(value)
+            }
+        },
+        mounted() {
+            this.tryInitSortable()
         }
     }
 </script>
 
-<style scoped>
+<style >
+    .el-table th:hover{
+        @apply bg-gray-100;
+        @apply cursor-pointer
+    }
+    .el-table th>.cell{
+        @apply flex;
+        @apply items-center;
+    }
 
 </style>
