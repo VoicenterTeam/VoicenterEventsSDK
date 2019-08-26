c4769797191ee65601320444176a37c2c423158b
Vadim Borodin <vadim.borodin@binarcode.com>
2019-08-26 10:55:57 +0300

    RTD-20: update tab color on hover&default

--- src/components/LayoutRendering/Sidebar.vue
+++ src/components/LayoutRendering/Sidebar.vue
@@ -1,8 +1,12 @@
 <template>
     <div class="w-full flex items-center sidebar-tabs__container">
         <div class="tab-name padding-x_56 font-medium text-lg cursor-pointer" v-for="group in widgetGroupList">
-            <p @click="switchTab(group.WidgetGroupID.toString())">{{group.WidgetGroupTitle}}</p>
-            <div v-if="group.WidgetGroupID.toString() === activeTab" class="self-border mt-3"></div>
+            <p @click="switchTab(group.WidgetGroupID.toString())"
+               :class="{'active': group.WidgetGroupID.toString() === activeTab}">{{group.WidgetGroupTitle}}
+            </p>
+            <div v-if="group.WidgetGroupID.toString() === activeTab"
+                 class="self-border mt-3">
+            </div>
         </div>
     </div>
 </template>
@@ -22,7 +26,7 @@
         },
     }
 </script>
-<style scoped>
+<style scoped lang="scss">
     .sidebar-tabs__container {
         position: absolute;
         margin-right: -6rem;
@@ -36,7 +40,11 @@
     .tab-name {
         height: 22px;
         line-height: normal;
-        color: var(--greyish-brown);
+        color: var(--steel);
+
+        :hover, .active {
+            color: var(--greyish-brown);
+        }
     }
 
     .self-border {
