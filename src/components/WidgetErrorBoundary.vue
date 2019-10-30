e6e5c5b97b888aadc3532d66af84a570d9e9e46e
cristij <joracristi@gmail.com>
2019-10-30 12:31:59 +0200

    RTD-136 Improve error boundary component to display more explicit errors

--- src/components/WidgetErrorBoundary.vue
+++ src/components/WidgetErrorBoundary.vue
@@ -1,18 +1,31 @@
 <script>
+    import get from 'lodash/get'
     export default {
         data: () => ({
-            error: false
+            error: false,
+            errorMessage: '',
+            errorInfo: '',
+            errorVm: ''
         }),
         errorCaptured(err, vm, info) {
+            let vmName = get(vm, '$options.name', '')
             this.error = true
+            this.errorMessage = `${err.message} on "${info}" inside "${vmName}" component`
+            this.errorInfo = info
+            this.errorVm = vm
             console.warn(err, vm, info)
         },
         render(h) {
+            let children = [
+                h('div', this.$t('widget.content.renderError')),
+                h('div', this.errorMessage),
+            ]
+            let extraClasses = this.error ? 'flex-col' : ''
             return this.error ? h('div', {
                 attrs: {
-                    class: 'text-lg flex justify-center items-center w-full h-32 text-red-500'
+                    class: `text-lg flex justify-center items-center w-full h-32 text-red-500 ${extraClasses}`
                 }
-            }, this.$t('widget.content.renderError')) : this.$slots.default[0]
+            }, children) : this.$slots.default[0]
         }
     }
 </script>
