4ae3c1a220a53f4aff079ce07b163918480b49e7
cristij <joracristi@gmail.com>
2019-10-30 13:58:12 +0200

    RTD-136 Extract function to get widget template

--- src/helpers/wigetUtils.js
+++ src/helpers/wigetUtils.js
@@ -2,22 +2,24 @@ import get from 'lodash/get'
 import store from '@/store/store'
 
 export function getWidgetEndpoint(widget) {
-    let getWidgetTemplate = store.getters['widgetTemplate/getWidgetTemplate']
     let endpoint = get(widget, 'WidgetLayout.Endpoint')
     if (!endpoint) {
-        let widgetTemplate = getWidgetTemplate(widget.TemplateID)
+        let widgetTemplate = getWidgetTemplate(widget)
         endpoint = get(widgetTemplate, 'Endpoint')
     }
     return endpoint.replace('{WidgetID}', widget.WidgetID)
 }
 
 export function getWidgetDataType(widget) {
-    let getWidgetTemplate = store.getters['widgetTemplate/getWidgetTemplate']
     let dataType = get(widget, 'WidgetLayout.DataTypeID')
     if (!dataType) {
-        let widgetTemplate = getWidgetTemplate(widget.TemplateID)
+        let widgetTemplate = getWidgetTemplate(widget)
         dataType = get(widgetTemplate, 'DataType.DataTypeID')
     }
     return dataType
 }
 
+export function getWidgetTemplate(widget) {
+    let getWidgetTemplate = store.getters['widgetTemplate/getWidgetTemplate']
+    return getWidgetTemplate(widget.TemplateID)
+}
