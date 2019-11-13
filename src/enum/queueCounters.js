02c7b78d5eb4c117a39311e8bf6e62eeff85156b
Vadim Borodin <vadim.borodin@binarcode.com>
2019-11-13 18:30:33 +0200

    RTD-147: update icons/colors

--- src/enum/queueCounters.js
+++ src/enum/queueCounters.js
@@ -4,15 +4,15 @@ export const typeKeys = {
     CALLERS_ID: 0,
     MAXIMUM_WAITING_ID: 1
 }
-
+//TODO: update icons if will be necessary
 export const types = {
     [`${typeKeys.CALLERS_ID}`]: {
         icon: "IconIncomingCall",
-        color: config.colors.info,
+        color: config.colors.light_green,
         text: 'queue.counter.callers'
     },
     [`${typeKeys.MAXIMUM_WAITING_ID}`]: {
-        icon: "IconTeamMeeting",
+        icon: "IconAdministrative",
         color: config.colors.default,
         text: 'queue.maximum.waiting.caller'
     },
