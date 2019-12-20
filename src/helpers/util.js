ebe5300466aa92249ac37047025e04eb81db8905
Vadim Borodin <vadim.borodin@binarcode.com>
2019-12-20 15:34:04 +0200

    RTD-155: Ability to update logo and title from settings

--- src/helpers/util.js
+++ src/helpers/util.js
@@ -1,3 +1,12 @@
 export function capitalizeFirstLetter(str) {
     return  str.charAt(0).toUpperCase() + str.slice(1)
 }
+
+export function getBase64(file) {
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => resolve(reader.result);
+        reader.onerror = error => reject(error);
+    });
+}
