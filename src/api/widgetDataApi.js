84669780cdb40aec74a89c5cc105e2b99f02d6eb
Vadim Borodin <vadim.borodin@binarcode.com>
2019-12-20 15:45:23 +0200

    RTD-176: set route type = get

--- src/api/widgetDataApi.js
+++ src/api/widgetDataApi.js
@@ -14,7 +14,7 @@ export const WidgetDataApi = {
 
     async getExternalData(endPoint) {
         try {
-            let res = await instance.post(endPoint)
+            let res = await instance.get(endPoint)
             return res.Data
         } catch (e) {
             parseCatch(e, true)
