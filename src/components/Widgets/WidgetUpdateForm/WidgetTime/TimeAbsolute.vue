8ededed6708ae1d430adba926a59d671903f65d3
Vadim Borodin <vadim.borodin@binarcode.com>
2020-01-08 17:39:50 +0200

    RTD-156: small fix

--- src/components/Widgets/WidgetUpdateForm/WidgetTime/TimeAbsolute.vue
+++ src/components/Widgets/WidgetUpdateForm/WidgetTime/TimeAbsolute.vue
@@ -45,7 +45,7 @@
         methods: {
             onChange(dates) {
                 let Difference_In_Time = new Date().getTime() - dates[0].getTime()
-                this.model.WidgetTime.datedeff = parseInt(Difference_In_Time / TIME_LINE_TIMESTAMP.DAY)
+                this.model.WidgetTime.datedeff = parseInt(Difference_In_Time / TIME_LINE_TIMESTAMP.DAY).toString()
 
                 let _Difference_In_Time = dates[1].getTime() - dates[0].getTime()
                 this.model.WidgetTime.Date_interval = parseInt(_Difference_In_Time / TIME_LINE_TIMESTAMP.DAY)
