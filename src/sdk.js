45eb25d64c2bbd3cbc1bf8df83b8b3cbe1270b77
cristij <joracristi@gmail.com>
2020-02-07 11:39:04 +0200

    RTD-189 Make sure to ignore server fetching if servers are passed as option via config

--- src/sdk.js
+++ src/sdk.js
@@ -250,6 +250,10 @@ class EventsSDK {
   }
 
   async _getServers() {
+    // Ignore server fetch if we have a list of servers passed via options
+    if (this.options.servers && Array.isArray(this.options.servers) && this.options.servers.length > 1) {
+      return
+    }
     try {
       let params = {};
       if (this.options.serverType) {
