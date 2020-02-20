157cacf0d7123ac3c9aea45f78134d619a8ddf50
Vadim Borodin <vadim.borodin@binarcode.com>
2020-02-20 21:23:50 +0200

    RTD-232: If the user is in edit widget dialog - dont refresh the data in the background

--- src/services/widgetService.js
+++ src/services/widgetService.js
@@ -46,7 +46,7 @@ export async function getWidgetData(widget) {
     if (isExternalDataWidget(widget)) {
         return await WidgetDataApi.getExternalData(widget.EndPoint)
     } else {
-        const bodyElement = document.querySelector("body");
+        let bodyElement = document.querySelector("body");
         if (bodyElement.classList.contains("el-popup-parent--hidden")) return;
 
         return await WidgetDataApi.getData(widget.EndPoint);
