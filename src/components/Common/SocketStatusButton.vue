fbf0599aabb57a2cb1d1db6a31d0d8359ba2d72a
cristij <joracristi@gmail.com>
2020-02-27 16:32:34 +0200

    RTD-231 Socket status button updates

--- src/components/Common/SocketStatusButton.vue
+++ src/components/Common/SocketStatusButton.vue
@@ -1,29 +1,41 @@
 <template>
     <el-tooltip class="item" effect="dark"
-                :content="isSocketOffline ? $t('tooltip.socket.reconnect'): $t('tooltip.socket.connected')"
+                :content="tooltipContent"
                 placement="bottom">
         <button class="btn p-2 shadow rounded bg-white flex items-center"
-                :class="{'text-red-500 hover:bg-red-100 flash-animation': isSocketOffline, 'text-green-500 hover:bg-primary-100': !isSocketOffline}"
-                :disabled="!isSocketOffline"
-                @click="$emit('click')">
+                :class="{'text-red-500 hover:bg-red-100 flash-animation': isSocketOffline, 'text-green-500': !isSocketOffline}"
+                @click="onClick">
             <ZapIcon class="w-5 h-5"/>
             <span v-if="isSocketOffline" class="ml-1">
-                                    {{$t('Reconnect')}}
-                                </span>
+                {{$t('Reconnect')}}
+            </span>
         </button>
     </el-tooltip>
 </template>
 <script>
+    import { Tooltip } from 'element-ui'
     import { ZapIcon } from 'vue-feather-icons'
 
     export default {
         name: 'socket-status-button',
         components: {
-            ZapIcon
+            ZapIcon,
+            [Tooltip.name]: Tooltip
         },
         computed: {
             isSocketOffline() {
                 return this.$store.getters['extensions/isSocketOffline']
+            },
+            tooltipContent() {
+                return this.isSocketOffline ? this.$t('tooltip.socket.reconnect') : this.$t('tooltip.socket.connected')
+            }
+        },
+        methods: {
+            onClick() {
+                if (this.isSocketOffline) {
+                    return
+                }
+                this.$emit('click')
             }
         }
     }
