fb3122e5ace65b9e0f74dd197103d208bb26f70b
cristij <joracristi@gmail.com>
2020-04-17 11:34:50 +0300

    Fix issue in filterQueuesByIds getter

--- src/store/queues.js
+++ src/store/queues.js
@@ -40,7 +40,7 @@ const getters = {
     if (!queueIds || !Array.isArray(queueIds)) {
       return state.all
     }
-    this.state.all.filter(e => queueIds.includes(e.QueueID))
+    state.all.filter(e => queueIds.includes(e.QueueID))
   }
 }
 export default {
