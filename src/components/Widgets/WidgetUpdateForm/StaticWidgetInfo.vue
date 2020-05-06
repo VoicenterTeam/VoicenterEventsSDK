4965d853d388448dd3550bc47f7639d8d90a0895
Vadim Borodin <vadim.borodin@binarcode.com>
2020-05-06 15:10:19 +0300

    [RTD-316]: Allow displaying Permissions Alert Icon in the header of the widget

--- src/components/Widgets/WidgetUpdateForm/StaticWidgetInfo.vue
+++ src/components/Widgets/WidgetUpdateForm/StaticWidgetInfo.vue
@@ -1,23 +1,34 @@
 <template>
+    <div class="flex">
     <el-popover
         placement="bottom-start"
         trigger="hover">
         <div v-if="widget.WidgetID">
             <p>{{$t('widget.id') + widget.WidgetID}}</p>
-            <p>{{$t('template.name') + get(widgetTemplate, 'TemplateName') + ', ' + get(widgetTemplate, 'TemplateID')}}</p>
+            <p>{{$t('template.name') + getTemplateName + ', ' + getTemplateID}}</p>
         </div>
         <InfoIcon slot="reference" class="text-primary cursor-help"></InfoIcon>
     </el-popover>
+        <el-popover
+            v-if="permissionsBlocked.length"
+            class="mx-1"
+            placement="bottom-start"
+            trigger="hover">
+            <div v-html="getPermissionsBlockedIds"/>
+            <AlertTriangleIcon slot="reference" class="text-orange-400 cursor-help"></AlertTriangleIcon>
+        </el-popover>
+    </div>
 </template>
 <script>
     import get from 'lodash/get'
     import {Popover} from 'element-ui'
-    import {InfoIcon} from 'vue-feather-icons'
+    import {InfoIcon, AlertTriangleIcon} from 'vue-feather-icons'
     import {getWidgetTemplate} from '@/helpers/widgetUtils'
 
     export default {
         components: {
             InfoIcon,
+            AlertTriangleIcon,
             [Popover.name]: Popover,
         },
         props: {
@@ -26,13 +37,23 @@
                 default: () => ({})
             }
         },
-        data() {
-            return {
-                widgetTemplate: getWidgetTemplate(this.widget)
+        computed: {
+            widgetTemplate() {
+                return getWidgetTemplate(this.widget)
+            },
+            getTemplateName() {
+                return get(this.widgetTemplate, 'TemplateName', '--')
+            },
+            getTemplateID() {
+                return get(this.widgetTemplate, 'TemplateID', '--')
+            },
+            permissionsBlocked() {
+                return get(this.widget, 'WidgetLayout.PermissionsBlocked', [])
+            },
+            getPermissionsBlockedIds() {
+                const permissionsBlocked = this.permissionsBlocked.join(', ')
+                return `${this.$t('This widget is displaying a partial list because of permissions issues.')} <br> ${this.$t('The following IDs were not displayed are')}: <b>${permissionsBlocked}</b>`
             }
         },
-        methods: {
-            get,
-        }
     }
 </script>
