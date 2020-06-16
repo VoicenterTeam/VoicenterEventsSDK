902df94b654889d00328f1e3804787a86bb86c17
Vadim Borodin <vadim.borodin@binarcode.com>
2020-06-17 00:21:02 +0300

    try to fix netlify deployment

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 let defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
+    purge: [
+        './src/**/*.html',
+        './src/**/*.vue',
+        './src/**/*.jsx',
+    ],
     theme: {
         extend: {
             colors: {
