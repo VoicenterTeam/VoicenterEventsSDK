f6f5f907c77570bf0fd37d671e3ee12e9590bdf2
VadimBorodin <vadimborodin02@gmail.com>
2021-03-09 17:22:36 +0200

    cleanups

--- src/api/dashboardApi.js
+++ src/api/dashboardApi.js
@@ -2,7 +2,7 @@ import $axios from './apiConnection'
 import store from '@/store/store'
 import parseCatch from '@/helpers/handleErrors'
 
-function currentAccountId () {
+function currentAccountId() {
     return store.state.entities.selectedAccountID
 }
 
@@ -20,24 +20,27 @@ async function userDashboards(dashboards) {
 export const DashboardApi = {
     async getAll() {
         try {
-            let {DashBoards} = await $axios.get('/DashBoards/GetAll/')
+            let { DashBoards } = await $axios.get('/DashBoards/GetAll/')
+            if (!currentAccountId()) {
+                return DashBoards
+            }
             return await userDashboards(DashBoards)
         } catch (e) {
-
+            
             const currentTime = (new Date().getTime() / 1000).toFixed(0)
-
+            
             if (currentTime - initialTime < 11) {
                 await timeout(2000)
                 return await this.getAll()
             }
-
+            
             parseCatch(e, true)
             setTimeout(() => {
                 window.location.href = process.env.VUE_APP_FALLBACK_URL
             }, 1000)
         }
     },
-
+    
     async find(id) {
         try {
             let res = await $axios.get(`/DashBoards/Get/${id}`)
@@ -46,7 +49,7 @@ export const DashboardApi = {
             parseCatch(e, true, 'Find Dashboard')
         }
     },
-
+    
     async update(data) {
         try {
             data.WidgetGroupList.forEach(group => {
@@ -55,19 +58,19 @@ export const DashboardApi = {
                         widget.WidgetConfig = []
                     }
                     widget.WidgetConfig.forEach(config => {
-                        if (config.WidgetParameterValue === "{}") {
+                        if (config.WidgetParameterValue === '{}') {
                             config.WidgetParameterValue = ''
                         }
                     })
                 })
             })
-
+            
             return await $axios.post(`/DashBoards/Update/`, data)
         } catch (e) {
             parseCatch(e, true, 'Update Dashboard')
         }
     },
-
+    
     async store(data) {
         try {
             return await $axios.post(`/DashBoards/Add/`, data)
@@ -75,7 +78,7 @@ export const DashboardApi = {
             parseCatch(e, true, 'Add Dashboard')
         }
     },
-
+    
     async addWidgetGroup(dashboardID, widgetGroupID) {
         try {
             return await $axios.post(`/DashBoards/AddWidgetGroup/${dashboardID}/${widgetGroupID}`)
@@ -83,7 +86,7 @@ export const DashboardApi = {
             parseCatch(e, true, 'Add widget group to dashboard')
         }
     },
-
+    
     async removeWidgetGroup(dashboardID, widgetGroupID) {
         try {
             return await $axios.post(`/DashBoards/RemoveWidgetGroup/${dashboardID}/${widgetGroupID}`)
@@ -91,7 +94,7 @@ export const DashboardApi = {
             parseCatch(e, true, 'Remove widget group from dashboard')
         }
     },
-
+    
     async destroy(dashboardID) {
         try {
             return await $axios.post(`/DashBoards/RemoveDashboard/${dashboardID}`)
