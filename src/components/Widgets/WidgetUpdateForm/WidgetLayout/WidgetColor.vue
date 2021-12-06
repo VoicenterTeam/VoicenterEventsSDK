32ba9921c897459bc556a0e88edd8cea16452c04
alexander-me <alexander.me@starkey.co.il>
2021-12-06 18:48:08 +0200

    [RTD-23]: - moved iterated functionality from WidgetColors to WidgetColor component;

---
+++ src/components/Widgets/WidgetUpdateForm/WidgetLayout/WidgetColor.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="flex items-center">
+        <slot>
+            <IconColorPicker class="w-6 h-6"
+                             :style="getIndicatorStyles(color)"/>
+        </slot>
+        <span class="px-1 text-main-sm">
+            {{ $t('widget.settings.color.' + option) }}
+        </span>
+        <color-picker
+            class="mx-2 mt-1-5"
+            show-alpha
+            v-model="color"
+            :predefine="predefinedColors"/>
+    </div>
+</template>
+
+<script>
+import ColorPicker from '@/components/Common/ColorPicker'
+
+export default {
+    components: {
+        ColorPicker
+    },
+    props: [
+        'color',
+        'option',
+        'predefinedColors'
+    ],
+    methods: {
+        getIndicatorStyles(color) {
+            return {
+                'color': color,
+            }
+        },
+    }
+}
+</script>
