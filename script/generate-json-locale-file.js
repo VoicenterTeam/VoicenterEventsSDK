68f1cd8a0436ef023afdf3f938889b22fce433ee
OrtZz <orest.zhurosmkyi@starkey.co.il>
2022-02-08 16:33:10 +0200

    added: checking if tagsList is exist

--- script/generate-json-locale-file.js
+++ script/generate-json-locale-file.js
@@ -3,12 +3,16 @@ const fs = require('fs')
 const path = require('path')
 const axios = require('axios').default;
 const apiUrl = process.env.VUE_APP_API_URL
-const defaultDomain = process.env.VUE_APP_DEFAULT_DOMAIN_NAME
+const defaultDomain = process.env.VUE_APP_DEFAULT_DOMAIN_NAME || 'dashboarddev.voicenter.co'
 const localeDir = path.join(__dirname, '../', 'src', 'locales')
 
 const operationsWithLocaleJsonFIle = async () => {
   try {
     const response = await axios.post(`${apiUrl}/Contents/TagsList/`, { Domain: defaultDomain });
+    if (!response.data.Data) {
+      return
+    }
+
     const data = JSON.stringify(response.data.Data, null, "\t");
 
     if (fs.existsSync(path.join(localeDir, 'en.json'))) {
