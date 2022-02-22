0588ab4d7b1e1b185086255d1325ac89dbde1724
serhii.ku <serhii.ku@voicenter.com>
2022-02-22 14:47:34 +0200

    refactored code

--- src/directives/ellipsis-check.js
+++ src/directives/ellipsis-check.js
@@ -1,30 +1,12 @@
 export default {
     bind: function (el, binding, vNode) {
         const inst = vNode.context;
-        //console.log('el', el)
-        //console.log('inst', inst)
-        /*el.addEventListener("mouseover", (e) => {
-            if (el.offsetWidth < el.scrollWidth) {
-                inst.isTruncated = true;
-            } else {
-                inst.isTruncated = false;
-            }
-        });*/
-        el.addEventListener("DOMSubtreeModified", (e) => {
-            console.log('DOMSubtreeModified')
+        el.addEventListener("mouseover", (e) => {
             if (el.offsetWidth < el.scrollWidth) {
                 inst.isTruncated = true;
             } else {
                 inst.isTruncated = false;
             }
         });
-        /*inst.$on('DOMSubtreeModified', function(){
-            console.log('changed');
-            if (el.offsetWidth < el.scrollWidth) {
-                inst.isTruncated = true;
-            } else {
-                inst.isTruncated = false;
-            }
-        });*/
     },
 }
