12b68107eaa15f9f2319ae7195037de3ae8b9892
Orest Zhuromskyi <orest.zhuromskyy@gmail.com>
2022-03-09 19:14:46 +0200

    deleted spaces

--- src/modules/reports/ReportsLayout.vue
+++ src/modules/reports/ReportsLayout.vue
@@ -1,334 +1,165 @@
 <template>
-
     <div>
-
         <BaseNavbar/>
-
         <report-tabs
-
             :dataTabs="dataTabs"
-
             :active-tab="activeTab"
-
             @update-active-tab="updateActiveTab"
-
             @on-remove-tab="removeTab">
-
             <template v-slot:list>
-
                 <list-temp
-
                     v-show="listTabName === activeTab"
-
                     @on-create-report="openCreateReportTab"
-
                     @on-edit-row="onEditRow"/>
-
             </template>
-
-
             <template v-for="tab in editableTabs"
-
-                      v-slot:[tab.name]="{data}">
-
+                v-slot:[tab.name]="{data}">
                 <edit-temp
-
                     v-show="tab.name === activeTab"
-
                     :data="tab.data"
-
                     :report-id="tab.name"
-
                     @update-title="onUpdateTitle"/>
-
             </template>
-
-
             <template v-slot:create>
-
                 <create-temp v-show="createTabName === activeTab"/>
-
             </template>
-
         </report-tabs>
-
     </div>
-
 </template>
 
 <script>
 
 import get from "lodash/get"
-
 import {TabPane, Tabs} from 'element-ui'
-
-
 import BaseNavbar from '@/components/Navbar/BaseNavbar'
-
 import ReportTabs from "@/modules/reports/components/ReportTabs";
-
-
 import EditTemp from "@/modules/reports/pages/ReportEdit";
-
 import ListTemp from "@/modules/reports/pages/ReportsList";
-
 import CreateTemp from "@/modules/reports/pages/ReportCreate";
-
 import { reportApi } from "./services/reportService"
-
-
 const CREATE_TAB_NAME = 'create';
-
 const LIST_TAB_NAME = 'list'
 
-
 export default {
-
     components: {
-
         BaseNavbar,
-
         [Tabs.name]:
-
         Tabs,
-
         [TabPane.name]:
-
         TabPane,
-
         ReportTabs,
-
         EditTemp,
-
         ListTemp,
-
         CreateTemp
-
     },
-
     data() {
-
         return {
-
             activeTab: LIST_TAB_NAME,
-
             dataTabs: [
-
                 {
-
                     title: 'Report List', // TODO: Use translation for Report List
-
                     name: LIST_TAB_NAME,
-
                     icon: 'vc-icon-template-list',
-
-                },
-
+                }
             ],
-
             createTabName: CREATE_TAB_NAME,
-
             listTabName: LIST_TAB_NAME
-
         }
-
     },
-
     computed: {
-
         editableTabs() {
-
             return this.dataTabs.filter(t => t.name !== LIST_TAB_NAME && t.name !== CREATE_TAB_NAME);
-
-        },
-
+        }
     },
-
     methods: {
-
         updateActiveTab(tab) {
-
             let routePath
-
             if (tab === LIST_TAB_NAME) {
-
                 routePath = '/reports'
-
             } else if (tab === CREATE_TAB_NAME) {
-
                 routePath = '/reports/create'
-
             } else {
-
                 routePath = `/reports/edit/${tab}`
 
             }
-
             if (this.$route.path === routePath) return
-
-
             this.$router.push(routePath)
-
         },
-
         removeTab(tab) {
-
             const deleteIndex = this.dataTabs.findIndex(t => t.name === tab)
-
             if (deleteIndex !== -1 && tab !== LIST_TAB_NAME) {
-
                 if (this.activeTab === tab) {
-
                     this.activeTab = LIST_TAB_NAME
-
                 }
-
                 this.dataTabs.splice(deleteIndex, 1)
-
             }
-
         },
-
         openCreateReportTab() {
-
             const createReportTab = this.dataTabs.find(tab => tab.name === CREATE_TAB_NAME)
-
             if (createReportTab) {
-
                 this.activeTab = createReportTab.name
-
             } else {
-
                 this.dataTabs.push({
-
                     title: 'Create',
-
                     name: CREATE_TAB_NAME,
-
                     icon: 'vc-icon-plus-linear',
-
                 })
-
-
                 this.$nextTick(() => {
-
                     this.activeTab = CREATE_TAB_NAME
-
                 })
-
             }
-
         },
-
         onUpdateTitle(id, title) {
-
             const updateIndex = this.dataTabs.findIndex(t => t.name === id)
-
-
             if (updateIndex !== -1) {
-
                 this.dataTabs[updateIndex].title = title
-
             }
-
         },
-
         onEditRow(row) {
-
             const tabName = get(row, 'ReportID', '').toString()
-
             const reportName = get(row, 'ReportName', '')
-
             const editTabExists = this.dataTabs.find(tab => tab.name === tabName)
-
-
             if (!editTabExists) {
-
                 this.dataTabs.push({
-
                     title: reportName,
-
                     name: tabName,
-
                     data: row
-
                 })
-
             }
-
-
             this.$nextTick(() => {
-
                 this.activeTab = tabName
-
             })
-
         }
-
     },
-
     watch: {
-
         '$route': {
-
             immediate: true,
-
             handler(route) {
-
                 const routePath = route.path
-
                 const routeEditId = route.params.id
-
                 const createRoute = '/reports/create'
-
-
                 if (routeEditId) {
-
                     const editTabExists = this.dataTabs.find(tab => tab.name === routeEditId)
-
-
                     if (!editTabExists) {
-
                         this.dataTabs.push({
-
                             title: routeEditId,
-
                             name: routeEditId
-
                         })
-
                     }
-
-
                     this.$nextTick(() => {
-
                         this.activeTab = routeEditId
-
                     })
-
                 } else if (routePath === createRoute) {
-
                     this.openCreateReportTab()
-
                 } else {
-
                     this.activeTab = LIST_TAB_NAME
-
                 }
-
             }
-
         }
-
     },
     async mounted () {
-
         const confData = await reportApi.getReportConfData()
-
         this.$store.dispatch('report/setConfData', confData)
-
     }
-
 }
-
 </script>
