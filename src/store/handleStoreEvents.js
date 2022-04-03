be62a2ee4b7823a9eefe6fc14bd0c4262133b132
OrtZz <orest.zhurosmkyi@starkey.co.il>
2022-04-03 14:49:38 +0300

    updated: updating dialers logic in handlerStoreEvents

--- src/store/handleStoreEvents.js
+++ src/store/handleStoreEvents.js
@@ -63,7 +63,8 @@ export default function onNewEvent({ eventData, store, extensionsModuleName, que
       store.dispatch(`${dialersModuleName}/setDialers`, data.dialers)
       break;
     case eventTypes.DIALER_EVENT:
-      store.dispatch(`${dialersModuleName}/updateDialers`, data.dialers)
+      let dialers = [data.data];
+      store.dispatch(`${dialersModuleName}/updateDialers`, dialers)
       break;
     default:
       break;
