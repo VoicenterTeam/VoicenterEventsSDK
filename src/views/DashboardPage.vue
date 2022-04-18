1906a13a5795984bf60d3100cad35d0d47e532f4
OrtZz <orest.zhurosmkyi@starkey.co.il>
2022-04-18 17:28:59 +0300

    deleted: unnecessary vuex persistent variables and added skeletons on page loading

--- src/views/DashboardPage.vue
+++ src/views/DashboardPage.vue
@@ -1,24 +1,78 @@
 <template>
-    <fade-transition mode="out-in" :duration="100">
-        <Dashboard :account-no-data="accountNoData"/>
-    </fade-transition>
+    <div>
+        <zoom-center-transition :duration="100">
+            <Dashboard :account-no-data="accountNoData" v-show="!isContentLoading"/>
+        </zoom-center-transition>
+        <el-skeleton
+            :loading="isContentLoading"
+            animated
+        >
+            <template slot="template">
+                <el-skeleton-item
+                    variant="h3"
+                    class="mb-3 w-full header-h"
+                />
+                <el-skeleton-item
+                    variant="h3"
+                    class="mb-10 w-full header-h"
+                />
+                <div
+                    class="px-10 w-full"
+                >
+                    <el-skeleton-item
+                        class="mb-4 widget-h"
+                    />
+                    <div
+                        class="flex items-center"
+                    >
+                        <el-skeleton-item
+                            variant="text"
+                            class="mr-8 widget-h"
+                        />
+                        <el-skeleton-item
+                            variant="text"
+                            class="widget-h w-2/5"
+                        />
+                    </div>
+                </div>
+            </template>
+        </el-skeleton>
+    </div>
 </template>
 <script>
+import { Skeleton, SkeletonItem } from 'element-ui'
     
     export default {
         components: {
             Dashboard: () => import('@/views/Dashboard'),
+            [Skeleton.name]: Skeleton,
+            [SkeletonItem.name]: SkeletonItem
         },
         computed: {
-            accountNoData() {
+            accountNoData () {
                 return !this.loadingData && !this.activeDashboard
             },
-            activeDashboard() {
+            activeDashboard () {
                 return this.$store.getters['dashboards/getActiveDashboard']
             },
-            loadingData() {
+            loadingData () {
                 return this.$store.state.dashboards.loadingData
             },
-        },
+            isContentLoading () {
+                return this.$store.state.dashboards.contentLoading
+            }
+        }
     }
 </script>
+
+<style lang="scss" scoped>
+$widget-height: 380px;
+$header-height: 64px;
+
+.widget-h {
+    height: $widget-height;
+}
+.header-h {
+    height: $header-height;
+}
+</style>
