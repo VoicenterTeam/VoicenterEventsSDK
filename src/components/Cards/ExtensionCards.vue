44f88d254d9a1ffb5f50e8b65ac66d289ec251b6
OrtZz <orest.zhurosmkyi@starkey.co.il>
2022-05-26 18:22:33 +0300

    changed position of manage columns button

--- src/components/Cards/ExtensionCards.vue
+++ src/components/Cards/ExtensionCards.vue
@@ -2,38 +2,40 @@
     <div>
         <portal :to="`widget-header__${data.WidgetID}`">
             <div class="flex w-full justify-end overflow-hidden items-center">
-                <el-select :placeholder="$t('general.sortBy')" v-model="sortBy" size="small">
+                <el-select :placeholder="$t('general.sortBy')" v-model="sortBy" size="small" class="select-type">
                     <el-option :key="option.label" v-bind="option" v-for="option in sortByOptions"/>
                 </el-select>
-                <div class="cursor-pointer hidden lg:block">
-                    <template v-if="showDropDown">
-                        <el-dropdown size="mini" class="drop-down-columns px-2 py-1-5 hover:bg-primary-100 rounded" trigger="click">
-                            <span class="flex items-center">
-                                <i class="vc-icon-filter text-xl text-primary" />
-                            </span>
-                            <el-dropdown-menu slot="dropdown" >
-                                <el-dropdown-item>
-                                    <div class="bg-white rounded mt-1 flex flex-col border-2">
-                                        <div class="bg-gray-200 rounded-t border-b-2">
-                                            <p class="p-2 text-main-sm font-medium">{{ layoutColumns }}
-                                                {{ $t('columns.layout') }}
-                                            </p>
-                                        </div>
-                                        <div class="w-full flex p-2 justify-between">
-                                            <i
-                                                v-for="index in maxLayoutColumns"
-                                                :key="index"
-                                                :class="{'bg-primary-100': index <= layoutColumns}"
-                                                @click="updateGrid(index)"
-                                                class="icon-square mx-margin--1"
-                                            />
-                                        </div>
+            </div>
+        </portal>
+        <portal :to="`widget-header__${data.WidgetID}-action`">
+            <div class="cursor-pointer hidden lg:block">
+                <template v-if="showDropDown">
+                    <el-dropdown size="mini" class="px-2 py-1-5 hover:bg-primary-100 rounded" trigger="click">
+                        <span class="flex items-center">
+                            <i class="vc-icon-filter text-xl text-primary" />
+                        </span>
+                        <el-dropdown-menu slot="dropdown" >
+                            <el-dropdown-item>
+                                <div class="bg-white rounded mt-1 flex flex-col border-2">
+                                    <div class="bg-gray-200 rounded-t border-b-2">
+                                        <p class="p-2 text-main-sm font-medium">{{ layoutColumns }}
+                                            {{ $t('columns.layout') }}
+                                        </p>
                                     </div>
-                                </el-dropdown-item>
-                            </el-dropdown-menu>
-                        </el-dropdown>
-                    </template>
-                </div>
+                                    <div class="w-full flex p-2 justify-between">
+                                        <i
+                                            v-for="index in maxLayoutColumns"
+                                            :key="index"
+                                            :class="{'bg-primary-100': index <= layoutColumns}"
+                                            @click="updateGrid(index)"
+                                            class="icon-square mx-margin--1"
+                                        />
+                                    </div>
+                                </div>
+                            </el-dropdown-item>
+                        </el-dropdown-menu>
+                    </el-dropdown>
+                </template>
             </div>
         </portal>
         <div>
@@ -244,10 +246,10 @@
         margin-bottom: 0;
     }
 }
-[dir="ltr"] .drop-down-columns {
-    @apply ml-2;
-}
-[dir="rtl"] .drop-down-columns {
+[dir="ltr"] .select-type {
     @apply mr-2;
+}
+[dir="rtl"] .select-type {
+    @apply ml-2;
 }
 </style>
