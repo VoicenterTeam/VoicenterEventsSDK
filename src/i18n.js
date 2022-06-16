565482b6b5e098ea301dd9397c861fed78bd3ea9
OrtZz <orest.zhurosmkyi@starkey.co.il>
2022-06-16 15:53:42 +0300

    deleted unnecessary code

--- src/i18n.js
+++ src/i18n.js
@@ -1,17 +1,16 @@
 import Vue from 'vue';
 import VueI18n from 'vue-i18n';
-import store from './store/store'
 
 Vue.use(VueI18n);
 
 function loadLocaleMessages() {
-  // if (process.env.NODE_ENV === 'development'){
   const locales = require.context(
   './locales',
     true,
     /[A-Za-z0-9-_,\s]+\.json$/i
   );
   const messages = {};
+
   locales.keys().forEach(key => {
     const matched = key.match(/([a-z0-9]+)\./i);
     if (matched && matched.length > 1) {
@@ -19,11 +18,8 @@ function loadLocaleMessages() {
       messages[locale] = locales(key);
     }
   });
-  console.log(messages, 'messages', store, 'translations')
-  return messages;
-  // }
 
-  // return {}
+  return messages;
 }
 
 export default new VueI18n({
