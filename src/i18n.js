a0547343ebcf9f53b1f70d6ab667e34db31a3fbc
OrtZz <orest.zhurosmkyi@starkey.co.il>
2022-06-16 15:14:24 +0300

    try to merge translations

--- src/i18n.js
+++ src/i18n.js
@@ -1,27 +1,29 @@
 import Vue from 'vue';
 import VueI18n from 'vue-i18n';
+import store from './store/store'
 
 Vue.use(VueI18n);
 
 function loadLocaleMessages() {
-  if (process.env.NODE_ENV === 'development'){
-    const locales = require.context(
-    './locales',
-      true,
-      /[A-Za-z0-9-_,\s]+\.json$/i
-    );
-    const messages = {};
-    locales.keys().forEach(key => {
-      const matched = key.match(/([a-z0-9]+)\./i);
-      if (matched && matched.length > 1) {
-        const locale = matched[1];
-        messages[locale] = locales(key);
-      }
-    });
-    return messages;
-  }
+  // if (process.env.NODE_ENV === 'development'){
+  const locales = require.context(
+  './locales',
+    true,
+    /[A-Za-z0-9-_,\s]+\.json$/i
+  );
+  const messages = {};
+  locales.keys().forEach(key => {
+    const matched = key.match(/([a-z0-9]+)\./i);
+    if (matched && matched.length > 1) {
+      const locale = matched[1];
+      messages[locale] = locales(key);
+    }
+  });
+  console.log(messages, 'messages', store, 'translations')
+  return messages;
+  // }
 
-  return {}
+  // return {}
 }
 
 export default new VueI18n({
