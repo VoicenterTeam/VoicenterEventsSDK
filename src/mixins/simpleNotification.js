cd2827bb468d02a83924039e61737481dc908596
OrtZz <orest.zhurosmkyi@starkey.co.il>
2022-07-11 15:40:52 +0300

    added ignoring for rtl node

--- src/mixins/simpleNotification.js
+++ src/mixins/simpleNotification.js
@@ -6,14 +6,16 @@ export default {
             dangerouslyUseHTMLString: true,
             title: data.title,
             message: `
-                <span class="simple-notification-container">
-                    <div>
-                        <i class="simple-notification-container--icon el-icon-${data.type}"></i>
-                    </div>
-                    <div class="mx-2">
-                        <span class="simple-notification-container--message">${data.message}</span>
-                    </div>
-                </span>
+                <bdi>
+                    <span class="simple-notification-container">
+                        <div>
+                            <i class="simple-notification-container--icon el-icon-${data.type}"></i>
+                        </div>
+                        <div class="mx-2">
+                            <span class="simple-notification-container--message">${data.message}</span>
+                        </div>
+                    </span>
+                </bdi>
             `
         })
     }
