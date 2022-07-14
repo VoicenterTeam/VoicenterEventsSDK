278740d56ec2d39c879a7b619e347c0197aca9cf
OrtZz <orest.zhurosmkyi@starkey.co.il>
2022-07-14 11:11:26 +0300

    changed: API placement

--- src/App.vue
+++ src/App.vue
@@ -23,6 +23,7 @@ export default {
     async created() {
         try {
             await this.$store.dispatch('dashboards/setContentLoading', true)
+            await this.$store.dispatch('entities/getEntitiesList')
 
             await this.$store.dispatch('lang/getLanguages')
             const activeLanguage = this.$store.getters['lang/getActiveLanguage'] || 'en'
@@ -35,7 +36,6 @@ export default {
                 this.$i18n.mergeLocaleMessage(locale, this.$store.state.lang.translations)
             }
 
-            await this.$store.dispatch('entities/getEntitiesList')
             await this.$store.dispatch('dashboards/getDashboards')
             await this.$store.dispatch('templatesCategory/getAllTemplatesCategory')
 
@@ -119,11 +119,13 @@ export default {
         activeDashboard: {
             immediate: true,
             deep: true,
-            handler: async function () {
-                await this.$store.dispatch('layout/setupActiveLayout')
-            },
-        },
-    },
+            handler: async function (oldVal, newVal) {
+                if (oldVal && newVal && oldVal.DashboardID !== newVal.DashboardID) {
+                    await this.$store.dispatch('layout/setupActiveLayout')
+                }
+            }
+        }
+    }
 }
 </script>
 
