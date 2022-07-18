2c8e322a9c10bcb00bf52d5d7d87e2273fa9235d
OrtZz <orest.zhurosmkyi@starkey.co.il>
2022-07-18 19:18:49 +0300

    fixed: showing callerphone

--- src/components/Cards/CallInfo.vue
+++ src/components/Cards/CallInfo.vue
@@ -2,13 +2,12 @@
     <div class="flex items-center justify-between mt-2">
         <div class="flex flex-col" v-if="!hideCallerInfo" :class="{'w-full': hideCallInfo}">
             <span
-                v-if="call.callerphone !== call.callername"
                 class="font-medium mb-1"
                 :style="fontSize"
             >
                 {{ call.callername }}
             </span>
-            <span class="text-gray-500" :style="fontSize"><bdi>+{{ call.callerphone }}</bdi></span>
+            <span v-if="isMobilePhoneNumber" class="text-gray-500" :style="fontSize"><bdi>+{{ call.callerphone }}</bdi></span>
         </div>
         <div v-if="!hideCallInfo" class="flex items-center justify-between direction-icon">
             <component :is="directionMappings[call.direction]" class="mx-1"/>
@@ -118,6 +117,10 @@
                 return {
                     'fontSize': fontSize
                 }
+            },
+            isMobilePhoneNumber () {
+                const regRule = /^[0-9+]+$/gm
+                return regRule.test(this.call.callerphone)
             }
         },
         watch: {
