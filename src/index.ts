8b2454dbfb11e4b5c6a87c1a047d5b5f9fd8a5bd
Ostap Gadzeman <ostap.gad@starkey.co.il>
2023-10-18 12:34:50 +0300

    deleted test code

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import type { ObjectUnknown } from './types/generic'
 import { SomeInterface } from './types/generic'
-import SomeModuel from './modules/someModuel'
 
 const data: ObjectUnknown = {
     name: 'Petro',
@@ -9,14 +8,5 @@ const data: ObjectUnknown = {
 export const data2: SomeInterface = {
     name: 'Petro',
 }
-
-export const someModuel = new SomeModuel('someModuel', {
-    someData: {
-        0: data2,
-        2: {
-            name: 'Petro',
-        }
-    }
-})
 
 export default data
