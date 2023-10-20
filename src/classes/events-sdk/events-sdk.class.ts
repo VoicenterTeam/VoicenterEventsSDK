da3c13cbb78cbacddc8e33262012387b278fb0ab
Ostap Gadzeman <ostap.gad@starkey.co.il>
2023-10-20 14:30:12 +0300

    working on connect method; find next available server method for connect added

--- src/classes/events-sdk/events-sdk.class.ts
+++ src/classes/events-sdk/events-sdk.class.ts
@@ -23,6 +23,7 @@ class EventsSdkClass {
     }
 
     private argumentOptions: EventsSdkOptions;
+    private servers: Server[] = [];
     private server: Server;
     private connected = false;
     private connectionEstablished = false;
@@ -45,23 +46,23 @@ class EventsSdkClass {
     private async connect (server: ServerParameter = ServerParameter.DEFAULT, skipLogin = false): Promise<void> {
         this.doConnectOnDisconnect = true;
 
-        // let serverToConnect = null;
+        let serverToConnect: Server = null;
 
         if (server === ServerParameter.DEFAULT) {
-            // serverToConnect = this._findCurrentServer();
+            serverToConnect = this.findCurrentServer();
         }
 
         if (server === ServerParameter.NEXT) {
-            // serverToConnect = this._findNextAvailableServer();
+            serverToConnect = this.findNextAvailableServer();
         }
 
         if (server === ServerParameter.PREVIOUS) {
             // serverToConnect = this._findMaxPriorityServer();
         }
 
-        // if (!serverToConnect) {
+        if (!serverToConnect) {
         // this.server = this._findCurrentServer();
-        // }
+        }
 
         // this._initSocketConnection();
 
@@ -76,6 +77,76 @@ class EventsSdkClass {
         }
 
         // await this.login(this.options.loginType);
+    }
+
+    private findCurrentServer () {
+        if (this.servers.length) {
+            this.server = this.servers[0];
+        }
+        if (!this.server) {
+            this.server = this.options.fallbackServer;
+        }
+        return this.server;
+    }
+
+    private findNextAvailableServer (): Server | null {
+        // this.log(INFO, 'Failover -> Trying to find another server');
+
+        const currentServerPriority = this.server.Priority;
+
+        const nextServerPriority = currentServerPriority + 1;
+
+        const nextServer = this.servers.find(server => server.Priority === nextServerPriority) || this.findMaxPriorityServer();
+
+        if (this.server.Domain !== nextServer.Domain) {
+            this.server = { ...nextServer };
+
+            // this.log(INFO, 'Failover -> Found new server. Connecting to it...', this.server);
+
+            return this.server;
+        }
+
+        return null;
+    }
+
+    private findMaxPriorityServer () {
+        // this.log(INFO, 'Fallback -> Trying to find previous server');
+
+        const maxPriorityServer = this.getServerWithHighestPriority(this.servers);
+
+        if (!this.server) {
+            this.server = maxPriorityServer;
+
+            return this.server;
+        }
+
+        if (this.server && maxPriorityServer.Domain !== this.server.Domain) {
+            this.server = maxPriorityServer;
+
+            // this.log(INFO, 'Fallback -> Trying to find previous server');
+
+            return this.server;
+        }
+        
+        return null;
+    }
+
+    public getServerWithHighestPriority (servers: Server[]): Server {
+        // Highest priority server is the one with lowest Priority value
+
+        let chosenServer: Server;
+
+        let maxPriority = Number.MAX_SAFE_INTEGER;
+
+        servers.forEach(server => {
+            if (server.Priority < maxPriority) {
+                maxPriority = server.Priority;
+
+                chosenServer = server;
+            }
+        });
+
+        return chosenServer;
     }
 }
 
