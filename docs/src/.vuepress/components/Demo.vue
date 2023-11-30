09bf734d3ac9c7a02c5cdd3474f49ff9f2727cc5
Bohdan Konechnyi <bohdan.konechniy@gmail.com>
2023-11-30 15:10:32 +0100

    Fix: auth test, updated demo page

--- docs/src/.vuepress/components/Demo.vue
+++ docs/src/.vuepress/components/Demo.vue
@@ -1,60 +1,108 @@
 <template>
     <div>
-        <h2>
-            Hello
-        </h2>
+        <p v-if="loading">
+            Loading...
+        </p>
 
-        <pre><code style="color: black">{{ JSON.stringify(events, null, 4) }}</code></pre>
+        <template v-else>
+            <div v-if="loggedId">
+                <pre><code style="color: black">{{ JSON.stringify(events, null, 4) }}</code></pre>
+            </div>
+
+            <div v-else>
+                <form @submit.prevent="login">
+                    <input type="text" v-model="token" placeholder="token" />
+
+                    <button type="submit">Login</button>
+                </form>
+            </div>
+        </template>
     </div>
 </template>
 
 <script lang="ts" setup>
-import { reactive } from 'vue'
+import {reactive, ref} from 'vue'
 import EventsSdkClass from '@/index'
-import { eventsSdkDefaultOptions } from '@/classes/events-sdk/events-sdk-default-options'
-import { EventsEnum } from '@/enum/events.enum'
-import { EventTypeData } from '@/types/events'
+import {eventsSdkDefaultOptions} from '@/classes/events-sdk/events-sdk-default-options'
+import {EventsEnum} from '@/enum/events.enum'
+import {EventTypeData} from '@/types/events'
 
-const sdk = new EventsSdkClass(eventsSdkDefaultOptions)
-
+/* Data */
+const token = ref('')
+const loading = ref(false)
+const loggedId = ref(false)
 const events = reactive<{ [K in EventsEnum]?: Array<EventTypeData<K>> }>({})
 
-sdk.init()
-
-sdk.socket?.on(
-    EventsEnum.QUEUE_EVENT,
-    (data) => {
-        console.log(EventsEnum.QUEUE_EVENT, data)
-
-        if (!events[EventsEnum.QUEUE_EVENT]) {
-            events[EventsEnum.QUEUE_EVENT] = []
-        }
-
-        events[EventsEnum.QUEUE_EVENT]?.push(data)
+/* Methods */
+function login() {
+    if (!token.value) {
+        return alert('Token is required')
     }
-)
-sdk.socket?.on(
-    EventsEnum.EXTENSION_EVENT,
-    (data) => {
-        console.log(EventsEnum.EXTENSION_EVENT, data)
 
-        if (!events[EventsEnum.EXTENSION_EVENT]) {
-            events[EventsEnum.EXTENSION_EVENT] = []
+    loading.value = true
+
+    const sdk = new EventsSdkClass({
+        ...eventsSdkDefaultOptions,
+        token: token.value,
+    })
+
+    sdk.init()
+
+    sdk.socket?.on(
+        EventsEnum.QUEUE_EVENT,
+        (data) => {
+            console.log(EventsEnum.QUEUE_EVENT, data)
+
+            if (!events[EventsEnum.QUEUE_EVENT]) {
+                events[EventsEnum.QUEUE_EVENT] = []
+            }
+
+            events[EventsEnum.QUEUE_EVENT]?.push(data)
         }
+    )
+    sdk.socket?.on(
+        EventsEnum.EXTENSION_EVENT,
+        (data) => {
+            console.log(EventsEnum.EXTENSION_EVENT, data)
 
-        events[EventsEnum.EXTENSION_EVENT]?.push(data)
-    }
-)
-sdk.socket?.on(
-    EventsEnum.ALL_DIALER_STATUS,
-    (data) => {
-        console.log(EventsEnum.ALL_DIALER_STATUS, data)
+            if (!events[EventsEnum.EXTENSION_EVENT]) {
+                events[EventsEnum.EXTENSION_EVENT] = []
+            }
 
-        if (!events[EventsEnum.ALL_DIALER_STATUS]) {
-            events[EventsEnum.ALL_DIALER_STATUS] = []
+            events[EventsEnum.EXTENSION_EVENT]?.push(data)
         }
+    )
+    sdk.socket?.on(
+        EventsEnum.ALL_DIALER_STATUS,
+        (data) => {
+            console.log(EventsEnum.ALL_DIALER_STATUS, data)
 
-        events[EventsEnum.ALL_DIALER_STATUS]?.push(data)
-    }
-)
+            if (!events[EventsEnum.ALL_DIALER_STATUS]) {
+                events[EventsEnum.ALL_DIALER_STATUS] = []
+            }
+
+            events[EventsEnum.ALL_DIALER_STATUS]?.push(data)
+        }
+    )
+
+    sdk.socket?.on(
+        EventsEnum.LOGIN_SUCCESS,
+        (data) => {
+            console.log(EventsEnum.LOGIN_SUCCESS, data)
+
+            loggedId.value = true
+            loading.value = false
+        }
+    )
+
+    setTimeout(
+        () => {
+            if (!loggedId.value) {
+                loading.value = false
+                alert('Login failed')
+            }
+        },
+        5000
+    )
+}
 </script>
--- src/classes/auth/auth.test.ts
+++ src/classes/auth/auth.test.ts
@@ -2,8 +2,10 @@ import AuthClass from '@/classes/auth/auth.class'
 import { eventsSdkDefaultOptions } from '@/classes/events-sdk/events-sdk-default-options'
 
 describe('AuthClass', () => {
+    const authClass = new AuthClass()
+
     it('should be cypher', () => {
-        const key = AuthClass.login(eventsSdkDefaultOptions)
+        const key = authClass.login(eventsSdkDefaultOptions)
         expect(typeof key).toBe('string')
     })
 })
