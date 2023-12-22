7db6fae018442f408e1f29fc30db8b3d6fed8a45
Ostap Gadzeman <ostap.gad@starkey.co.il>
2023-12-22 13:50:10 +0200

    queue event typed

--- src/types/queue-event.d.ts
+++ src/types/queue-event.d.ts
@@ -13,22 +13,14 @@ export interface QueueEvent {
 interface QueueEventData {
     QueueID: number,
     QueueName: string,
-    Calls: [
-        {
-            CallerID: '0547649040',
-            CallerName: 'תמיכה ותקלות',
-            IvrUniqueID: '2023113013195102398b4b08508dc48a',
-            JoinTimeStamp: 1701350463,
-            calldurationinterval: 1701350463,
-            ivrid: '2023113013195102398b4b08508dc48a'
-        },
-        {
-            CallerID: '0508424551',
-            CallerName: 'Clicked Callback from site  ENG',
-            IvrUniqueID: '202311301322360239e8eeeedaf0153d',
-            JoinTimeStamp: 1701350557,
-            calldurationinterval: 1701350557,
-            ivrid: '202311301322360239e8eeeedaf0153d'
-        }
-    ]
+    Calls: QueueEventDataCall[]
+}
+
+interface QueueEventDataCall {
+    CallerID: string,
+    CallerName: string,
+    IvrUniqueID: string,
+    JoinTimeStamp: number,
+    calldurationinterval: number,
+    ivrid: string
 }
