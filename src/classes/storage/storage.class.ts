2fc2b68cb1b00a4496363820b8afd3dd0ba06c18
Ostap Gadzeman <ostap.gad@starkey.co.il>
2024-02-01 13:18:26 +0200

    changed code logic to avoid window not defined error

--- src/classes/storage/storage.class.ts
+++ src/classes/storage/storage.class.ts
@@ -2,14 +2,6 @@ import { LoginSessionData } from '@/types/auth'
 
 export class StorageClass{
     public static async getSessionStorageDataByKey (key: string): Promise<LoginSessionData | undefined> {
-        if (window) {
-            const loginSessionKey = window.sessionStorage.getItem(key)
-
-            if (loginSessionKey) {
-                return JSON.parse(loginSessionKey)
-            }
-        }
-
         if (chrome.storage) {
             const loginSessionKey = await chrome.storage.session.get(key)
 
@@ -17,27 +9,35 @@ export class StorageClass{
                 return JSON.parse(loginSessionKey[key])
             }
         }
+
+        if (window) {
+            const loginSessionKey = window.sessionStorage.getItem(key)
+
+            if (loginSessionKey) {
+                return JSON.parse(loginSessionKey)
+            }
+        }
     }
 
     public static async updateSessionStorageKey (key: string, storageData: LoginSessionData) {
-        if (window) {
-            window.sessionStorage.setItem(key, JSON.stringify(storageData))
-        }
-
         if (chrome.storage) {
             await chrome.storage.session.set({
                 [key]: JSON.stringify(storageData)
             })
         }
+
+        if (window) {
+            window.sessionStorage.setItem(key, JSON.stringify(storageData))
+        }
     }
 
     public static clearSessionStorage () {
-        if (window) {
-            window.sessionStorage.clear()
-        }
-
         if (chrome.storage) {
             chrome.storage.session.clear()
+        }
+
+        if (window) {
+            window.sessionStorage.clear()
         }
     }
 }
