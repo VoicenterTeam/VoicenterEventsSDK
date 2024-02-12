437a021999b1b273adaa5e04dfd737af8c93fa7a
Bohdan Konechnyi <bohdan.konechniy@gmail.com>
2024-02-12 13:54:20 +0100

    Fix: build

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import EventsSdkClass from '@/classes/events-sdk/events-sdk.class'
 import { EventsEnum } from '@/enum/events.enum'
 
+// Export all types and enums
 export type * from './types/auth'
 export type * from './types/events.common'
 export type * from './types/events'
 export type * from './types/listeners'
 export type * from './types/public-api'
 export type * from './types/socket'
+export type * from './classes/events-sdk/events-sdk.types'
 
 export { EventsEnum }
 
