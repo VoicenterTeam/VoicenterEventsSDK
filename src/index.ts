e5323fbe84c80cc56ad4df93c2a85346d5e617df
Ostap Gadzeman <ostap.gad@starkey.co.il>
2024-03-05 18:22:59 +0200

    real time events types deleted local types which are used from the package

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,10 @@ import EventsSdkClass from '@/classes/events-sdk/events-sdk.class'
 
 // Export all types and enums
 export type * from './types/auth'
-export type * from './types/events.common'
 export type * from './types/events'
 export type * from './types/listeners'
 export type * from './types/public-api'
 export type * from './types/socket'
 export type * from './classes/events-sdk/events-sdk.types'
-
-export * from './enum/events.enum'
 
 export default EventsSdkClass
