d5b919d3158eac31e66dbf1ee5aadef721608ce2
Ostap Gadzeman <ostap.gad@starkey.co.il>
2024-05-15 12:44:20 +0300

    sdk options interfaces to handle type depending on a login type

--- src/classes/events-sdk/events-sdk.types.ts
+++ src/classes/events-sdk/events-sdk.types.ts
@@ -2,37 +2,58 @@ import { Socket } from 'socket.io-client'
 import { LoginType } from '@/enum/auth.enum'
 import StorageLogger, { LoggerOptions } from '@voicenter-team/socketio-storage-logger'
 
-export interface EventsSdkOptions {
+interface EvetsSdkOptionsBase {
     isNewStack?: boolean,
+    loginType: LoginType,
+}
+
+interface EventsSdkOptionsLoginUser extends EvetsSdkOptionsBase {
+    loginType: LoginType.USER,
+    email: string,
+    password: string,
+}
+
+interface EventsSdkOptionsLoginToken extends EvetsSdkOptionsBase {
+    loginType: LoginType.TOKEN
+    token: string
+}
+
+type Options = EventsSdkOptionsLoginUser | EventsSdkOptionsLoginToken
+
+function a (options: Options) {
+    console.log(a)
+}
+a({
+    email: 'asd',
+    loginType: LoginType.TOKEN,
+    token: '123' 
+})
+
+export interface EventsSdkOptions {
     servers?: Server[],
     loginUrl: string,
     getSettingsUrl?: string
-    refreshTokenUrl: string,
+    refreshTokenUrl?: string,
     refreshToken?: string,
-    token: string,
     tokenExpiry?: Date,
-    loginType: LoginType,
     forceNew?: boolean,
-    reconnectionDelay: number,
+    reconnectionDelay?: number,
     reconnectionDelayMax?: number,
-    maxReconnectAttempts: number,
+    maxReconnectAttempts?: number,
     timeout?: number,
-    keepAliveTimeout: number,
+    keepAliveTimeout?: number,
     idleInterval?: number,
-    protocol: string,
+    protocol?: string,
     transports?: string[],
     upgrade?: boolean,
     serverFetchStrategy?: string,
     serverType?: number,
     useLogger?: boolean,
     loggerSocketConnection?: Socket,
-    loggerServer: string,
-    loggerConfig: LoggerOptions,
+    loggerServer?: string,
+    loggerConfig?: LoggerOptions,
     loggerConnectOptions?: LoggerConnectOptions,
-    email: string,
-    password: string,
-    username: string,
-    storageLoggerInstance: StorageLogger | undefined
+    storageLoggerInstance?: StorageLogger | undefined
 }
 
 export interface Server {
