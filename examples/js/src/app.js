0a383f90fd91d7232d99850ddc186fd0bd54c385
Ostap Gadzeman <ostap.gad@starkey.co.il>
2025-04-01 16:21:24 +0300

    useLogger false in the js examole to avoid error localStorage not defined

--- examples/js/src/app.js
+++ examples/js/src/app.js
@@ -6,13 +6,14 @@ import EventsSdk from '@voicenter-team/events-sdk'
         isNewStack: true,
         token: 'GvNkq0UFK3CcQiOujr705yIWITClJcy1kBRW2dajxg6c9GlWkPHCQDVfwcfG0xnQiT5NEMvspo0F7kCFbyvPjID9wknEVhy3mGyC',
         getSettingsUrl: 'https://loginapidev.voicenter.co.il/Application/GetSettings',
-        loginUrl: "https://loginapidev.voicenter.co.il/Auth/Login/Voicenter/Monitor",
-        refreshTokenUrl: "https://loginapidev.voicenter.co.il/Auth/RefreshToken",
+        loginUrl: 'https://loginapidev.voicenter.co.il/Auth/Login/Voicenter/Monitor',
+        refreshTokenUrl: 'https://loginapidev.voicenter.co.il/Auth/RefreshToken',
+        useLogger: false,
     })
 
-    await sdk.init();
+    await sdk.init()
 
-    sdk.on("*", (data) => {
-        console.log(`Event: ${data.name}`)
+    sdk.on('*', (data) => {
+        console.log(`Event: ${data.name}`, data)
     })
 })()
